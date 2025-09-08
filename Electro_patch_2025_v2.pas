c39b080ecd3b56f76c0982432d4ac077ca6839ea
Ivan Trunaev <trunaev.ivan@icloud.com>
2025-09-08 20:32:55 +0500

    Create Electro_patch_2025_v2.pas
    Patched version of Electro_patch_v2 for 2025 year (Old Moon Calendar).
    
    GPU, Officer Trunaev, Russia, Uray.
    
    Please use with saving of Copyright.

---
+++ Electro_patch_2025_v2.pas
@@ -0,0 +1,30 @@
+program Electro_patch_2025_v2;
+const 
+    float Freq := 120;
+    float R := 1;
+var 
+    double t, y;
+    float HalfFreq;
+    integer Step;
+    short j;
+begin;
+    HalfFreq := Freq / 2;
+    readln (t); t := t + 24*60*60*2;
+    if ((x = round (t)) and ((round (t) div HalfFreq) = 0) 
+       then begin;
+          y := 0;
+       else begin;
+          Step := t / HalfStep;
+          if ((Step / 2) = 0)
+          then
+              j :=1;
+          else
+              j := -1;
+          end;
+          t := j * sqrt ((R*R) - (t*t));
+        end;
+    writeln (t);
+end;
+
+ 
+;; by Tovarisch Trunaev, 2025, Russia, Uray
