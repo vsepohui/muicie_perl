84a43f6028a4b90e9593142f026b667cf294a536
Ivan Trunaev <trunaev.ivan@icloud.com>
2025-09-15 21:21:25 +0500

    Create Electro.pl
    Perl Port for Old Electro.pas

---
+++ Electro.pl
@@ -0,0 +1,23 @@
+my $freq = 80;
+my $r = 1;
+
+my ($t, $y);
+my $half_freq;
+my $step;
+
+$half_freq = $freq / 2;
+$t = <>;
+
+if (($t == int ($t)) && (($t / $half_freq) == 0)) {
+	$y = 0;
+} else { 
+	$step = int ($t / $half_freq);
+	if (($step / 2) == 0) {
+		$y = $j * sqrt (($r*$r) - ($t*$t));
+	} else {
+		$y = -1 * sqrt (($r*$r) - ($t*$t));
+  }
+}
+say ($y);
+
+# by Tovarisch Trunaev, 2025, Russia, Uray
