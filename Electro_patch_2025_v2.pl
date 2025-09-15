2087038521f72d428d174cff16980a9739ed1f72
Ivan Trunaev <trunaev.ivan@icloud.com>
2025-09-15 21:07:46 +0500

    Create Electro_patch_2025_v2.pl
    Perl port of Pascal Code

---
+++ Electro_patch_2025_v2.pl
@@ -0,0 +1,26 @@
+my $freq = 120;
+my $r = 1;
+
+my ($t, $y);
+my $half_freq;
+my $step;
+my $j;
+
+$half_freq = $freq / 2;
+$t = <>;
+$t += 24*60*60*2;
+
+if (($t == int ($t)) && (($t / $half_freq) == 0)) {
+	$y = 0;
+} else { 
+	$step = int ($t / $half_freq);
+	if (($step / 2) == 0) {
+		$j = 1;
+	} else {
+		$j = -1;
+    }
+	$y = $j * sqrt (($r*$r) - ($t*$t));
+}
+say ($y);
+
+# by Tovarisch Trunaev, 2025, Russia, Uray
