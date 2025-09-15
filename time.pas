f06a70794e1bdf3c2f9ffd55ecd0997b5825b294
Ivan Trunaev <trunaev.ivan@icloud.com>
2025-09-15 19:22:56 +0500

    Create time.pas

---
+++ time.pas
@@ -0,0 +1,18 @@
+Program Time;
+Const 
+   Freq = 120;
+Var 
+   o, t : Real;
+Begin
+   ReadLn(t);
+   if (t > 0) 
+      then
+          o := Freq / t;
+      else 
+          o := -1;
+      end;
+
+   WriteLn(o);
+End.
+
+{ Code by GPU Officer Yahwe, Toverisch Trunaev, Russia } 
