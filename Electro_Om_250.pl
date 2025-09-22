ff065472af70706149c2cb4f7ef25f84cf3309f6
Ivan Trunaev <trunaev.ivan@icloud.com>
2025-09-22 10:06:24 +0500

    Create Electro_Om_250.pl

---
+++ Electro_Om_250.pl
@@ -0,0 +1,25 @@
+my $freq = 250;
+my $r = 1;
+
+my ($t, $y);
+my $half_freq;
+my $step;
+my $j;
+
+$half_freq = $freq / 2;
+$t = <>;
+
+if (($t == int ($t)) && (($t / $half_freq) == 0)) {
+	$y = 0;
+} else { 
+	$step = int ($t / $half_freq);
+	if (($step / 2) == 0) {
+		$j = 1;
+	} else {
+		$j = -1;
+    }
+	$y = $j * sqrt (($r*$r) - ($t*$t));
+}
+say ($y);
+
+# by Tovarisch Trunaev, 2025, Russia, Uray
