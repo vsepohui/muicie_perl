5e6f024e9efa781814adaeb8892c142870896c6f
Ivan Trunaev <trunaev.ivan@icloud.com>
2025-09-22 10:05:22 +0500

    Create Electro_Om_250.pas

---
+++ Electro_Om_250.pas
@@ -0,0 +1,32 @@
+Program Electro_Om_250;
+Const 
+    Freq = 250;
+    R = 1;
+Var 
+    t, y : Real;
+    HalfFreq : Real;
+    Step : Integer;
+    j : Smallint;
+Begin;
+    HalfFreq := Freq / 2;
+    ReadLn (t);
+    if ((t = Trunc (t)) and ((t / HalfFreq) = 0))
+       then begin
+          y := 0;
+       end
+       else 
+       begin
+          Step := Trunc (t / HalfFreq);
+          if ((Step / 2) = 0)
+          then
+              j :=1;
+          else
+              j := -1;
+          end;
+          y := j * sqrt ((R*R) - (t*t));
+       end; 
+    WriteLn (y);
+end.
+
+ 
+{ by Tovarisch Trunaev, 2025, Russia, Uray }
