7a52209f46a9b9697ede89343b49af4e63c4ab73
Ivan Trunaev <trunaev.ivan@icloud.com>
2025-09-26 17:07:56 +0500

    Create Buddha_8bit.pas

---
+++ Buddha_8bit.pas
@@ -0,0 +1,13 @@
+Program Buddha_8bit;
+Const 
+    Step = 5;
+Var 
+    i, o : Real;
+Begin;
+    ReadLn (i)
+    o := i*5;
+	o := Trunc(o);
+    WriteLn (o);
+end.
+
+{ by Tovarisch Trunaev, 2025, Russia, Uray }
