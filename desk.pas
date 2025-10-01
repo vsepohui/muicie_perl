f2e77106cb14f38e11ee25d5bd09c215f4a27682
Ivan Trunaev <trunaev.ivan@icloud.com>
2025-10-01 14:21:23 +0500

    Create desk.pas

---
+++ desk.pas
@@ -0,0 +1,27 @@
+Program Desk;
+Var 
+    input: string;
+    output: string;
+    num, part, len: string;
+    t: integer;
+Begin;
+    ReadLn (input);
+    t := Pos(input, '.');
+    num := Copy(input, 1, t - 1);
+    len := Length(input);
+    If (len > Length(num) + 1)
+		Then begin
+			part := Copy(input, t, len);
+			If (Length(part) = 1)
+				then begin
+					part += '0';
+				end;
+		else begin
+			part := '00';
+	end;
+	output := num + part;
+    WriteLn (output);
+end.
+
+{ by Tovarisch Trunaev, 2025, Russia, Uray }
+{ Om->Delf(); }
