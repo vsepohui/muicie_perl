6552e9e9ab8a6538edeb369231d4072df8b8ff67
Ivan Trunaev <trunaev.ivan@icloud.com>
2025-10-06 12:11:30 +0500

    Create Keyboard.pas

---
+++ Keyboard.pas
@@ -0,0 +1,16 @@
+Program Keyboard;
+Var
+  i : integer;
+  sign : string;
+  c : char;
+Begin
+  ReadLn(i);
+  ReadLn(sign);
+  
+  c := chr(i);
+  WriteLn(c);
+  WriteLn(sign);
+End.
+
+{ Tovarisch Trunaev, GPU Officer Yahwe, Russian, Uray }
+{ Om->Delf() }
