c99b6da5df3e8046b9c315daf74701f5603b92f3
Ivan Trunaev <trunaev.ivan@icloud.com>
2025-10-06 10:58:44 +0500

    Create mighty-screen.pl

---
+++ mighty-screen.pl
@@ -0,0 +1,50 @@
+my $contrast = 255;   # Need to rewrite on update Firmware
+my $transparanse = 0; # Need to rewrite on update Firmware
+
+my $colors;
+my $sign;
+    
+my ($rs, $rg, $rb);
+my ($pos_r, $pos_g);
+my ($r, $g, $b);
+my $sum;
+
+$colors = <>;
+$sign = <>;
+chomp $colors;
+
+($r, $g, $b) = split /,/, $colors;
+$sum = $r + $g + $b;
+	
+say $contrast; # 1
+say $r;        # 2
+say $sum / 255; # 3 	
+say $transparanse; # 4 
+
+# 5
+if (($r > $g) && ($r > $b)) {
+  say $r;
+} else {
+  if ($g > $b) {
+    say $g;
+  } else {
+    say $b;
+  }
+}	
+
+# 6
+if (($r < $g) && ($r < $b)) {
+  say $r;
+} else {
+  if ($g < $b) {
+    say $g;
+  } else {
+    say $b;
+  }
+}	
+
+say $g;      # 7
+print $sign; # 8
+
+# by Tovarisch Trunaev, 2025, Russia, Uray
+{ Om->Delf(); }
