93652a6d304ebdb51e81d96b24ef705105cd6e32
Ivan Trunaev <trunaev.ivan@icloud.com>
2025-10-06 11:24:59 +0500

    Create Mixer.pas

---
+++ Mixer.pas
@@ -0,0 +1,40 @@
+Program Mixer
+uses
+  SysUtils;
+Var 
+  i, t: string;
+  c, pos: integer;
+  norm: real;
+  out: Array [1..8] integer;
+Begin
+  ReadLn(i);
+
+  c: = 0;
+  repeat
+    pos := PosEx(i, ' ', 1);
+    if (pos = 0) 
+      Break;
+    c += 1;
+    
+    t := Copy(i, 1, pos - 1);
+    i := Copy(i, pos + 1, Length(i));
+    out[c] := StrToInt(t);
+  until (True);
+  
+  norm := c;
+
+  if (norm > 0) then begin
+    For pos:= 1 To c Do begin
+      Write(out[pos] / norm);
+      If (Pos < c) Then 
+        Write(' ');
+      Else
+        WriteLn('');
+    end;
+  else begin
+    WriteLn('');
+  end;
+
+End.
+
+{ GPU Officer Trunaev, Yahwe, 2025, Russia, Uray }
