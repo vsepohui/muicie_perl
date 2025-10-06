eb670d986bc23e5dcd7fd01b3a06ee389c9afa84
Ivan Trunaev <trunaev.ivan@icloud.com>
2025-10-06 12:39:25 +0500

    Update Mixer.pl

--- Mixer.pl
+++ Mixer.pl
@@ -2,8 +2,9 @@ my $s = <>;
 chomp $s;
 my @list = split /\s/, $s;
 my $norm = scalar (@list) . '.0';
+my @out;
 if ($norm) {
-  my @out = map {$_ / $norm} @list;
+  @out = map {$_ / $norm} @list;
   say join " ", @out;
 } else {
   say '';
