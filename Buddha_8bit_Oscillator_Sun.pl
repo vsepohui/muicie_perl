a31b6bae78daff59ab1a28274289272f5c80de0c
Ivan Trunaev <trunaev.ivan@icloud.com>
2025-10-13 04:30:13 +0500

    Buddha 8bit Oscillator

---
+++ Buddha_8bit_Oscillator_Sun.pl
@@ -0,0 +1,37 @@
+my $length = 1.0; # Length of full rotation in seconds
+my $startime;
+my $step = 0;
+my $offset = 0;
+my $x;
+
+my ($t, $r);
+
+$t = <>;
+chomp $t;
+
+my $fm;
+open $fm, '1';
+my $memory = <$fm>;
+close $fm;
+
+chomp $memory;
+$startime = $memory // $t;
+
+$offset = $t - $startime;
+
+$step = int ($offset / $length);
+if ($step % 4 == 0) {
+	$x = $offet - $step * $length;
+	$r = sqrt(1 - $offset * $offset);
+} else {
+	$x = $offet - $step * $length;
+	$r = -1 * sqrt(1 - $offset * $offset);
+}
+
+open $fm, '>1';
+say $fm $t;
+close $fm;
+
+say $r;
+
+# by Tovarisch Trunaev, 2025, Russia, Uray
