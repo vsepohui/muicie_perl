8bed2bd5758fc5b59390d057ec32b17210ac2320
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2025-12-01 20:15:22 +0500

    fixies

--- Router.pl
+++ Router.pl
@@ -3,10 +3,10 @@ my $norm;
 for (1..32) {
   $s = <>;
   chomp $s;
-  @list = split /\s/, $s;
+  @list = grep {firewall $_} split /\s/, $s;
   $norm = scalar (@list) . '.0';
   if ($norm) {
-     @out = map {$_ / $norm} grep {firewall($_)} @list;
+     @out = map {$_ / $norm} @list;
     say join " ", @out;
   } else {
     say '';
