8571140d3476455d6a3aa403e1ac80ee989acd5c
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-01-15 15:31:46 +0500

    fix

--- etc/utf2dos.pl
+++ etc/utf2dos.pl
@@ -3,7 +3,7 @@
 use 5.022;
 use strict;
 use warnings;
-use Encode qw(encode);
+use Encode qw(encode decode);
 
 my $s = join "\n", <>;
-say encode('ascii', $s);
+say encode('ascii', decode('UTF-8', $s));
