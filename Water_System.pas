f94508c195bd5f1edcffb88c83e1dd067a50bd1f
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-04-17 03:49:32 +0500

    Sepated Power Production for Water System

--- Water_System.pas
+++ Water_System.pas
@@ -1,13 +1,17 @@
 Program Buddha_8bit;
 Const 
+	Min = 0.5;
     Median = 0.81;
-    Power = 0.8;
+    MaxPower = 1;
+    MinPower = 0.8;
 Var 
     w : Real;
 Begin;
     ReadLn (w)
-    IF (w < Median) Then
-		WriteLn (Power);
+    IF (w <= Min) Then
+		WriteLn (MaxPower);
+	ELSE IF (w < Median) Then
+		WriteLn (MinPower);
 end.
 
 { by Tovarisch Trunaev, 2026, Russia, Uray }
