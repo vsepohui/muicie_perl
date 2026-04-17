edbd3863b8169977ded0aa531f5bcf2c51b01697
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-04-17 08:13:31 +0500

    Add GSM Firmware

---
+++ GSM.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use 5.022;
+use warnings;
+
+my $transmit = <>;
+chomp $transmit;
+
+if ($transmit) {
+	my $id = <>;
+	chomp $id;
+	my $signal = <>;
+	chomp $signal;
+	
+	srand(hex $id);
+	my $freq = int (rand(440000)) / 10.0;
+	
+	my $i = int $freq;
+	my $freq_cut = $i == $freq ? $i . '.' : $freq;
+	
+	say $freq;
+	say $signal;
+	say '' . $freq_cut;
+}
+
+# by Tovarisch Trunaev, 2026, Russia, Uray
