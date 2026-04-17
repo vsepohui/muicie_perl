945d9457930a2090e2ad6bbc6238a68ba36d2eb6
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-04-17 07:26:42 +0500

    SMS Chips Firmware basecode

---
+++ SMS.pas
@@ -0,0 +1,39 @@
+Program Roaming;
+Uses
+	SysUtils;
+Var 
+	msg, hex : string;
+
+function CRC16(const Msg: string): Word;
+var
+  i, j: Integer;
+  crc: Word;
+begin
+  crc := $FFFF;
+  for i := 1 to Length(Msg) do
+  begin
+	crc := crc xor Word(Byte(Msg[i]));
+	for j := 0 to 7 do
+	begin
+	  if (crc and $0001) <> 0 then
+		crc := (crc shr 1) xor $A001;
+	  else
+		crc := crc shr 1;
+	end;
+  end;
+  Result := crc;
+end;
+	
+Begin;
+    ReadLn (msg);
+    
+    if (Length(msg) > 160) Then
+		msg := Copy(msg, 1, 160);
+    
+    hex := IntToHex(CRC16(msg));
+	
+	WriteLn (msg);
+	WriteLn (hex);
+end.
+
+{ by Tovarisch Trunaev, 2026, Russia, Uray }
