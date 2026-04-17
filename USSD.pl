8bcf4d36905fff73434520377f7f004c17e677a3
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-04-17 07:33:01 +0500

    USSD CPU Firmware

---
+++ USSD.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $cmd = <>;
+chomp $cmd;
+
+my $s = sprintf('%X', ord '*');
+my $h = sprintf('%X', ord '#');
+
+$cmd =~ s/\*/$s/g;
+$cmd =~ s/\#/$h/g;
+
+say $cmd;
+
+1;
+
+# by Tovarisch Trunaev, 2026, Russia, Uray
