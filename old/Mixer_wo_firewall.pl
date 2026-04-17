aefceb60aac37527731aed74563a88ee44e60c88
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-04-17 09:44:51 +0500

    Mixed w/o firewall

---
+++ old/Mixer_wo_firewall.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+use 5.022;
+
+no warnings "firewall"; # comment this line for debug parasite traffics in console: may be much tail...
+
+my $s = <>;
+chomp $s;
+my @list = split /\s/, $s;
+my $norm = scalar (@list) . '.0';
+my @out;
+if ($norm) {
+  @out = map {$_ / $norm} @list;
+  say join " ", @out;
+} else {
+  say '';
+}
+
+# GPU Officer Trunaev, Yahwe.
