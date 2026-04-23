0b052570bf285305b2154fa0284828bcb80e09b5
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-04-24 03:26:34 +0500

    Recover losed

---
+++ old/Mixer_wo_firewall.pl
@@ -0,0 +1,17 @@
+#Mixer [without_firewall]
+
+use 5.022;
+
+my $s = <>;
+chomp $s;
+my @list = split /\s/, $s;
+my $norm = scalar (@list) . '.0';
+my @out;
+if ($norm) {
+  @out = map {$_ / $norm} @list;
+  say join " ", @out;
+} else {
+  say '';
+}
+
+# GPU Officer Trunaev, Yahwe.
---
+++ old/Router_wo_firewall.pl
@@ -0,0 +1,20 @@
+#Router [without_firewall]
+
+use 5.022;
+
+my (@list, @out);
+my $norm;
+for (1..32) {
+  $s = <>;
+  chomp $s;
+  @list = split /\s/, $s;
+  $norm = scalar (@list) . '.0';
+  if ($norm) {
+     @out = map {$_ / $norm} @list;
+    say join " ", @out;
+  } else {
+    say '';
+  }
+}
+
+# GPU Officer Trunaev, Yahwe.
