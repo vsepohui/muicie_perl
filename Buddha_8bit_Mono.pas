5e81036fb8c5fc952d60d10f63039003cd4bedf2
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-04-25 22:49:02 +0500

    8 Bit Mono Distortion

--- Buddha_8bit_Mono.pas
+++ Buddha_8bit_Mono.pas
@@ -3,14 +3,14 @@ program Buddha_8_Bit_Mono;
 { 8 Bit Mono Distortion }
 
 Uses SysUtils;
-Const Step = 5;
-Var i, o : float;
+Const Step = 5.0;
+Var signal: float;
 
 Begin 
-	ReadLn (i);
-	i := i * Step;
-	o := Round (i) / Step;
-	WriteLn (o);
+	ReadLn (signal);
+	signal := signal * Step;
+	signal := Round (signal) / Step;
+	WriteLn (signal);
 	
 End.
 { by Tovarisch Trunaev, 2026, Russia, Uray }
